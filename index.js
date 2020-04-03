03cf4699e83d5c7c218aaca8f6e297c4a7f24678
Antonio Campos <tonnycav@hotmail.com>
2020-04-02 18:07:00 -0600

    resuelto

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const chords = require('./chords')
 
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "*")
   next()
 })
