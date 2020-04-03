ba3c74f1639e24aab8b6f7f3f83607adf4be6b0f
ulises-manrique <ulises.manrique@hotmail.com>
2020-04-03 15:08:08 -0600

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const chords = require('./chords')
 const scales = require('./scales')
-
+const port = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "*")
@@ -17,6 +17,7 @@ app.get('/scales', function (req, res) {
   res.json(scales.scales)
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('I wanna be yours')
+  console.log(`You´re in port: ${port}`)
 })
