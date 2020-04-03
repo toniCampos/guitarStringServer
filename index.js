9bcb85d404994cf3fefd947aacea0bdf2e97800b
ulises-manrique <ulises.manrique@hotmail.com>
2020-04-02 21:11:37 -0600

    se agrego la constante 'port'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const chords = require('./chords')
+const port = process.env.PORT || 3000 
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); 
@@ -13,6 +14,6 @@ app.get('/chords', function (req, res) {
   res.json(chords.chords)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`'Example app listening on port ${port}!'`)
 })
