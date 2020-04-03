dd9f28822f0ad87894695943b3dfca2483126c6b
ulises-manrique <ulises.manrique@hotmail.com>
2020-04-02 19:29:43 -0600

    modificacion de indes.js

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const chords = require('./chords')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+})
+
 app.get('/chords', function (req, res) {
+  console.log('enviado')
   res.json(chords.chords)
-});
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-});
+})
